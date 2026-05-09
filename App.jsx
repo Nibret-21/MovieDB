d53fe7e709e9f53380418e303444b386ba55c6a1
Nibret-21 <nibretadamu654@gmail.com>
2026-05-09 17:11:53 +0300

    update app and main jsx

--- App.jsx
+++ App.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import Header from './Components/Header/Header'
-import Banner from './Components/Banner/Banner'
-import DisplayRow from './Components/DisplayRow/DisplayRow'
-import Footer from './Components/Footer/Footer'
+// import React from 'react'
+// import Header from './Components/Header/Header'
+// import Banner from './Components/Banner/Banner'
+// import DisplayRow from './Components/DisplayRow/DisplayRow'
+// import Footer from './Components/Footer/Footer'
+
+// function App() {
+//   return (
+//     <div>
+//       <Header/>
+//       <Banner/>
+//       <DisplayRow/>
+//       <Footer/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./Components/Header/Header";
+import Banner from "./Components/Banner/Banner";
+import DisplayRow from "./Components/DisplayRow/DisplayRow";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Banner/>
-      <DisplayRow/>
-      <Footer/>
-    </div>
-  )
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Banner />
+              <DisplayRow />
+            </>
+          }
+        />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
-
+export default App;
